416f3ac1b10dccddb80b637713f3633caff48483
Jeff Grubb <jeffgrubb78@gmail.com>
2022-09-30 12:31:20 +0000

    sonarr, radarr, dupe scripts

--- createlinks.sh
+++ createlinks.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
 walk_dir () {
-    shopt -s nullglob dotglob
+  shopt -s nullglob dotglob
+  for pathname in "$1"/*; do
+    if [ -d "$pathname" ]; then
+      walk_dir "$pathname"
+    else
+      case "$pathname" in
+        *.mkv|*.avi|*.mpg)
+          printf '%s\n' "$pathname"
+          filename1=$(basename "$pathname")
+          printf '%s\n' "$filename1"
 
-    for pathname in "$1"/*; do
-        if [ -d "$pathname" ]; then
-            walk_dir "$pathname"
-        else
-            case "$pathname" in
-                *.mkv|*.avi)
-                    printf '%s\n' "$pathname"
-                    filename1=$(basename "$pathname")
-                    printf '%s\n' "$filename1"
+          duplicate=$(find /plex/library/tv -type f -name "$filename1" | wc -l)
+          echo $duplicate
+          if [ $duplicate == 1 ]; then
+            echo "Do Something with the duplicate here"
+            filename2=$(find /plex/library/tv -type f -name "$filename1")
+            echo "Main file $filename2"
+            echo "Duplicate file $pathname"
 
-                    duplicate=$(find /plex/library/tv -type f -name "$filename1" | wc -l)
-                    echo $duplicate
-                    if [ $duplicate == 1 ]; then
-                      echo "Do Something with the duplicate here"
-                      filename2=$(find /plex/library/tv -type f -name "$filename1")
-                      echo "Main file $filename2"
-   
-                      $(mv $pathname newnamefile)
-                      echo "The file is renamed."
-                    fi
-            esac
-        fi
-    done
+            if [[ ! -L "$pathname" ]]; then
+              # Check the file is exists or not
+              if [ -f "$pathname" ] ; then
+                rm "$pathname"
+              fi
+              echo "$pathname is removed"
+              ln -s "$filename2" "$pathname"
+            fi
+          fi
+        ;;
+      *.png|*.nfo)
+      ;;
+      *)
+        echo "File Extension: ${pathname##*.}"
+        #echo "some other extension"
+      ;;
+      esac
+    fi
+  done
 }
 
 DOWNLOADING_DIR=/plex/library/downloads
