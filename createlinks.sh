889a8de551902f077a324adb1cd0c169c2a87d24
Jeff Grubb <jeffgrubb78@gmail.com>
2022-09-30 13:15:29 +0000

    sonarr, radarr, dupe scripts

--- createlinks.sh
+++ createlinks.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+unlinkDup () {
+  #  $1 -> Filename that we are searching for
+  #  $2 -> Directory that we are searching in
+
+  filename1=$(basename "$1")  ##Get the filename for searching in the directory tree
+  duplicate=$(find "$2" -type f -name "$filename1" | wc -l)
+  if [ $duplicate == 1 ]; then
+    filename2=$(find "$2" -type f -name "$filename1")
+    echo "Main file $filename2"
+    echo "Duplicate file $1"
+
+    # Check the file is exists or not
+    if [ -f "$1" ] ; then
+      rm "$1"
+    fi
+    echo "$1 is removed"
+    ln -s "$filename2" "$1"
+    linkdetails=$(ls -l $1)
+    echo "New Link Created -> $linkdetails"
+  fi
+
+}
+
+
 walk_dir () {
   shopt -s nullglob dotglob
   for pathname in "$1"/*; do
@@ -8,29 +32,12 @@ walk_dir () {
     else
       case "$pathname" in
         *.mkv|*.avi|*.mpg)
-          printf '%s\n' "$pathname"
-          filename1=$(basename "$pathname")
-          printf '%s\n' "$filename1"
-
-          duplicate=$(find /plex/library/tv -type f -name "$filename1" | wc -l)
-          echo $duplicate
-          if [ $duplicate == 1 ]; then
-            echo "Do Something with the duplicate here"
-            filename2=$(find /plex/library/tv -type f -name "$filename1")
-            echo "Main file $filename2"
-            echo "Duplicate file $pathname"
-
-            if [[ ! -L "$pathname" ]]; then
-              # Check the file is exists or not
-              if [ -f "$pathname" ] ; then
-                rm "$pathname"
-              fi
-              echo "$pathname is removed"
-              ln -s "$filename2" "$pathname"
-            fi
+          if [[ ! -L "$pathname" ]]; then
+            unlinkDup "$pathname" "/plex/library/tv"
+            unlinkDup "$pathname" "/plex/library/movies"
           fi
         ;;
-      *.png|*.nfo)
+      *.png|*.nfo|*.part|*.rar|*.sfv|*.r0*|*.r1*)
       ;;
       *)
         echo "File Extension: ${pathname##*.}"
@@ -40,6 +47,7 @@ walk_dir () {
     fi
   done
 }
+
 
 DOWNLOADING_DIR=/plex/library/downloads
 walk_dir "$DOWNLOADING_DIR"
